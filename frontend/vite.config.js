89639809ad96bd7155665821c784ccb1e7d7a49a
GuntreddyHemanth <guntreddyhemanth@gmail.com>
2024-12-21 12:17:18 +0530

    adding vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,16 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/", // Use "/" if deploying to Vercel
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  build: {
+    outDir: "dist", // Explicitly specify the output directory
+  },
+});
